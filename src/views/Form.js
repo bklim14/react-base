72448028168177d259fd2f2fa7475565d5875777
Brian Lim <brian.lim.14@gmail.com>
2020-07-24 18:03:13 +0800

    Added nested routes

--- src/views/Form.js
+++ src/views/Form.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import {Link, Switch, Route, useRouteMatch, Redirect} from "react-router-dom";
 
-class Form extends React.Component {
-  render() {
-    return <div >Form Works!</div>;
-  }
+const Form = () =>  {
+  let { path, url } = useRouteMatch();
+  console.log(path, url)
+  return (
+    <div>
+      <Link to={`${url}/create`}>
+        <button className="btn btn-primary">
+          Create
+        </button>
+      </Link>
+      <Switch>
+        <Route path={`${path}/create`}>
+          <FormCreate />
+        </Route>
+        <Route path={`${path}/search`}>
+          <FormSearch url={url} />
+        </Route>
+        <Redirect from={path} to={`${path}/search`} />
+      </Switch>
+    </div>
+  );
+}
+
+const FormCreate = ({url}) => {
+  return (
+    <div>
+      <Link to={`${url}/search`}>
+        <button className="btn btn-primary">
+          Back
+        </button>
+      </Link>
+      Create
+    </div>
+  )
+}
+
+const FormSearch = () => {
+  return <div>Search</div>
 }
 
 export default Form;
