f7b0cbe3340472ce33685705587ee11799a00045
Brian Lim <brian.lim.14@gmail.com>
2020-07-24 15:27:51 +0800

    Updates

---
+++ precache-manifest.401b46368727f468d6c057697b5a763c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb7aa1bedf53f927c313fd3c0fb83de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d34f08e3a8d2e79143be",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0f9afdee63e5003ed2d7",
+    "url": "/static/css/main.951545c7.chunk.css"
+  },
+  {
+    "revision": "d34f08e3a8d2e79143be",
+    "url": "/static/js/2.a603fe21.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/static/js/2.a603fe21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9afdee63e5003ed2d7",
+    "url": "/static/js/main.6ba0dab9.chunk.js"
+  },
+  {
+    "revision": "32f52754b6c1f2e84842",
+    "url": "/static/js/runtime-main.8be7c7f3.js"
+  }
+]);
