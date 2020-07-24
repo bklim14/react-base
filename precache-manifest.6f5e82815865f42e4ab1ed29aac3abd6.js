11ecbc8aaa2627f9f12160c72348a6d4e6fd42da
Brian Lim <brian.lim.14@gmail.com>
2020-07-24 15:39:45 +0800

    Updates

---
+++ precache-manifest.6f5e82815865f42e4ab1ed29aac3abd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570db5d3b0cbb5e4e984d07cc5ec21c5",
+    "url": "/react-base/index.html"
+  },
+  {
+    "revision": "d34f08e3a8d2e79143be",
+    "url": "/react-base/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0f9afdee63e5003ed2d7",
+    "url": "/react-base/static/css/main.951545c7.chunk.css"
+  },
+  {
+    "revision": "d34f08e3a8d2e79143be",
+    "url": "/react-base/static/js/2.a603fe21.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/react-base/static/js/2.a603fe21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9afdee63e5003ed2d7",
+    "url": "/react-base/static/js/main.6ba0dab9.chunk.js"
+  },
+  {
+    "revision": "e53320226c8c4d86349f",
+    "url": "/react-base/static/js/runtime-main.9e32eb70.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.401b46368727f468d6c057697b5a763c.js"
+  "/react-base/precache-manifest.6f5e82815865f42e4ab1ed29aac3abd6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-base/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
